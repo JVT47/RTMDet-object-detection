ed04b8b7d6d9f131f36ec787a542f506f97e559e
Joona Tammela <joona.tammela@gmail.com>
2024-12-06 22:24:17 +0200

    Add log iou cost implementation

---
+++ src/losses/log_iou_cost.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+
+class LogIoUCost(nn.Module):
+    """
+    The regression cost function used in RTMDet paper.
+    """
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+    
+    def forward(self, IoU: torch.Tensor) -> torch.Tensor:
+        """
+        IoU: tensor of shape (n). Should contain the IoU score between the target bbox and bbox predictions.
+        """
+        loss = torch.log(IoU)
+        
+        return loss
---
+++ tests/test_losses/test_log_iou_cost.py
@@ -0,0 +1,15 @@
+import torch
+
+from src.losses.log_iou_cost import LogIoUCost
+
+
+class TestLogIoUCost:
+    log_iou_cost = LogIoUCost()
+
+    def test_forward(self) -> None:
+        iou = torch.tensor([0.5, 1.0, 0.1])
+
+        cost = self.log_iou_cost(iou)
+        target = torch.tensor([-0.693, 0.0, -2.30])
+
+        torch.testing.assert_close(cost, target, atol=1e-2, rtol=0.0)
