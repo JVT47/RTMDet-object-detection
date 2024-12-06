0680964443659a66c3d02bf49f413fa4c431712c
Joona Tammela <joona.tammela@gmail.com>
2024-12-06 19:51:43 +0200

    Add some tests for RTMDetOutput

--- tests/test_processors/test_rtmdet_postprocessor.py
+++ tests/test_processors/test_rtmdet_postprocessor.py
@@ -1,5 +1,6 @@
 import torch
 
+from tests.utils import create_model_output_1
 from src.dataclasses.bbox_label_container import BBoxLabelContainer
 from src.dataclasses.rtmdet_output import RTMDetOutput
 from src.processors.rtmdet_postprocessor import RTMDetPosprocessor
@@ -34,23 +35,7 @@ class TestRTMDetPostprocesor:
         torch.testing.assert_close(detection_result.scores, torch.tensor([0.8, 0.9]))
     
     def test_process_batch(self) -> None:
-        cls_1 = torch.ones((2, 4, 4, 3)) * -1
-        cls_1[0, 1, 1] = torch.tensor([0, 10, 5])
-        cls_1 = cls_1.permute(0, 3, 1, 2)
-        cls_2 = torch.ones((2, 2, 2, 3)) * -1
-        cls_2[1, 1, 1] = torch.tensor([10, 0, -10])
-        cls_2 = cls_2.permute(0, 3, 1, 2)
-        cls_3 = torch.ones((2, 3, 1, 1)) * -1
-
-        reg_1 = torch.zeros((2, 4, 4, 4))
-        reg_1[0, 1, 1] = torch.tensor([1, 1, 0, 0])
-        reg_1 = reg_1.permute(0, 3, 1, 2)
-        reg_2 = torch.zeros((2, 2, 2, 4))
-        reg_2[1, 1, 1] = torch.tensor([0, 0, 1, 1])
-        reg_2 = reg_2.permute(0, 3, 1, 2)
-        reg_3 = torch.zeros((2, 4, 1, 1))
-
-        model_output = RTMDetOutput((cls_1, cls_2, cls_3), (reg_1, reg_2, reg_3))
+        model_output = create_model_output_1()
 
         detection_results = self.postprocessor.process_batch(model_output)
 
---
+++ tests/utils.py
@@ -0,0 +1,25 @@
+import torch
+
+from src.dataclasses.rtmdet_output import RTMDetOutput
+
+def create_model_output_1() -> RTMDetOutput:
+    """
+    Creates a mock output of a RTMDet model. 
+    """
+    cls_1 = torch.ones((2, 4, 4, 3)) * -1
+    cls_1[0, 1, 1] = torch.tensor([0, 10, 5])
+    cls_1 = cls_1.permute(0, 3, 1, 2)
+    cls_2 = torch.ones((2, 2, 2, 3)) * -1
+    cls_2[1, 1, 1] = torch.tensor([10, 0, -10])
+    cls_2 = cls_2.permute(0, 3, 1, 2)
+    cls_3 = torch.ones((2, 3, 1, 1)) * -1
+
+    reg_1 = torch.zeros((2, 4, 4, 4))
+    reg_1[0, 1, 1] = torch.tensor([1, 1, 0, 0]) # bbox [7, 7, 8, 8]
+    reg_1 = reg_1.permute(0, 3, 1, 2)
+    reg_2 = torch.zeros((2, 2, 2, 4))
+    reg_2[1, 1, 1] = torch.tensor([0, 0, 1, 1]) # bboc [16, 16, 17, 17]
+    reg_2 = reg_2.permute(0, 3, 1, 2)
+    reg_3 = torch.zeros((2, 4, 1, 1))
+
+    return RTMDetOutput((cls_1, cls_2, cls_3), (reg_1, reg_2, reg_3))
