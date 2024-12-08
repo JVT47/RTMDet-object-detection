Fix negative sign error in log iou loss

src/losses/log_iou_cost.py
@@ -13,6 +13,6 @@ class LogIoUCost(nn.Module):
         """
         IoU: tensor of shape (n). Should contain the IoU score between the target bbox and bbox predictions.
         """
-        loss = torch.log(IoU)
+        loss = -torch.log(IoU)
         
         return loss

tests/test_losses/test_log_iou_cost.py
@@ -10,6 +10,6 @@ class TestLogIoUCost:
         iou = torch.tensor([0.5, 1.0, 0.1])
 
         cost = self.log_iou_cost(iou)
-        target = torch.tensor([-0.693, 0.0, -2.30])
+        target = torch.tensor([0.693, 0.0, 2.30])
 
         torch.testing.assert_close(cost, target, atol=1e-2, rtol=0.0)