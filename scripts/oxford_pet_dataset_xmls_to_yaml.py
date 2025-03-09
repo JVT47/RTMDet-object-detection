ae0cc52c4de413aa574d1e9e6659eeddf617405b
JVT47 <122315156+JVT47@users.noreply.github.com>
2025-03-09 20:06:05 +0200

    Automatically split Oxford Pet Dataset to train, valid, and test sets

--- scripts/oxford_pet_dataset_xmls_to_yaml.py
+++ scripts/oxford_pet_dataset_xmls_to_yaml.py
@@ -1,9 +1,10 @@
 """
-Script used to convert xml annotations for the Oxford-IIIT Pet Dataset into single yaml file.
+Script used to convert xml annotations for the Oxford-IIIT Pet Dataset into train, test, and valid yaml files.
 """
 
 from argparse import ArgumentParser
 from pathlib import Path
+import random
 import re
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import Element
@@ -110,7 +111,15 @@ def clean_annotation_dict(annotation: dict) -> dict:
     return result
 
 
-def xmls_to_yaml(xmls_path: Path, save_file_path: Path) -> None:
+def save_to_yaml(save_file_path: Path, annotations: list[dict]) -> None:
+    yaml_dict = {"annotations": annotations}
+    with open(save_file_path, "w") as f:
+        yaml.dump(data=yaml_dict, stream=f, default_flow_style=False)
+
+
+def xmls_to_yaml(
+    xmls_path: Path, save_dir_path: Path, test_portion: float, valid_portion: float
+) -> None:
     annotations = []
     for xml_file in xmls_path.glob("*.xml"):
         tree = ET.parse(xml_file)
@@ -122,10 +131,18 @@ def xmls_to_yaml(xmls_path: Path, save_file_path: Path) -> None:
 
         annotations.append(clean_annotation_dict(annotation))
 
-    yaml_dict = {"annotations": annotations}
+    random.shuffle(annotations)
+    n_annotations = len(annotations)
 
-    with open(save_file_path, "w") as f:
-        yaml.dump(data=yaml_dict, stream=f, default_flow_style=False)
+    train_upper_index = int(test_portion * n_annotations)
+    valid_upper_index = int((test_portion + valid_portion) * n_annotations)
+
+    save_to_yaml(save_dir_path.joinpath("train.yaml"), annotations[:train_upper_index])
+    save_to_yaml(
+        save_dir_path.joinpath("valid.yaml"),
+        annotations[train_upper_index:valid_upper_index],
+    )
+    save_to_yaml(save_dir_path.joinpath("test.yaml"), annotations[valid_upper_index:])
 
 
 def main() -> None:
@@ -136,15 +153,44 @@ def main() -> None:
         "--xmls-dir", required=True, help="Path to the dir with the xml annotations"
     )
     arg_parser.add_argument(
-        "--save-file-path", required=True, help="File path to the resulting yaml file"
+        "--save-dir-path",
+        required=True,
+        help="Dir path where the train.yaml, test.yaml, and valid.yaml are saved",
+    )
+    arg_parser.add_argument(
+        "--train-portion",
+        default=0.7,
+        type=float,
+        help="Portion of data used for training data.",
+    )
+    arg_parser.add_argument(
+        "--valid-portion",
+        default=0.2,
+        type=float,
+        help="Portion of data used for validation data.",
+    )
+    arg_parser.add_argument(
+        "--seed",
+        default=42,
+        type=int,
+        help="Seed used to initialize the random number generator. Different seed produces different train, test, and valid split",
     )
 
     args = arg_parser.parse_args()
 
     xmls_path = Path(args.xmls_dir)
-    save_file_path = Path(args.save_file_path)
+    save_dir_path = Path(args.save_dir_path)
+    train_portion = args.train_portion
+    valid_portion = args.valid_portion
 
-    xmls_to_yaml(xmls_path, save_file_path)
+    if (train_portion < 0 or valid_portion < 0) or train_portion + valid_portion > 1:
+        raise ValueError(
+            "train-portion and valid-portion must be nonnegative and their sum must be at most 1."
+        )
+
+    random.seed(args.seed)
+
+    xmls_to_yaml(xmls_path, save_dir_path, train_portion, valid_portion)
 
 
 if __name__ == "__main__":
